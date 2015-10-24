278eba94880f6408f34a409bfbba4464b846075f
davy engone <davy@philos.io>
2015-10-24 08:35:39 +0200

    removed ui-router

--- app/modules/book/book.configuration.js
+++ app/modules/book/book.configuration.js
@@ -3,61 +3,60 @@
 
 	function configuration($routeProvider, $compileProvider, $stateProvider){
 
-		$stateProvider
-			.state('home', {
-				url: '/',
-				controller:'BookController',
-				controllerAs: 'vm',
-				templateUrl: 'templates/main.html'
-			})
-			.state('books', {
-				// abstract: true,
-				url: "/books", 
-				controller:'BookController',
-				controllerAs: 'vm',
-				templateUrl: 'templates/main.html'
-			})
-			.state('books.add', {
-				url:'/add',
-				controller:'BookController',
-				controllerAs: 'vm',
-				templateUrl: 'templates/addBook.html'
-			})
-			.state('books.details', {
-				url:'/details/:bookID',
-				controller: 'BookDetailsController', // TODO: bookdetailscontroller
-				controllerAs: 'vm',
-				templateUrl: 'templates/bookDetails.html'
-			});
-
-
-		$compileProvider.debugInfoEnabled(false);
-		// $routeProvider
-		// 	.when('/', {
+		// $stateProvider
+		// 	.state('home', {
+		// 		url: '/',
 		// 		controller:'BookController',
 		// 		controllerAs: 'vm',
 		// 		templateUrl: 'templates/main.html'
 		// 	})
-		// 	.when('/books', {
+		// 	.state('books', {
+		// 		// abstract: true,
+		// 		url: "/books", 
 		// 		controller:'BookController',
 		// 		controllerAs: 'vm',
 		// 		templateUrl: 'templates/main.html'
 		// 	})
-		// 	.when('/books/add', {
+		// 	.state('books.add', {
+		// 		url:'/add',
 		// 		controller:'BookController',
 		// 		controllerAs: 'vm',
 		// 		templateUrl: 'templates/addBook.html'
 		// 	})
-		// 	.when('/books/:bookID', {
+		// 	.state('books.details', {
+		// 		url:'/details/:bookID',
 		// 		controller: 'BookDetailsController', // TODO: bookdetailscontroller
 		// 		controllerAs: 'vm',
 		// 		templateUrl: 'templates/bookDetails.html'
 		// 	});
-	}	
+    //
+
+		$compileProvider.debugInfoEnabled(false);
+		$routeProvider
+			.when('/', {
+				controller:'BookController',
+				controllerAs: 'vm',
+				templateUrl: 'templates/main.html'
+			})
+			.when('/books', {
+				controller:'BookController',
+				controllerAs: 'vm',
+				templateUrl: 'templates/main.html'
+			})
+			.when('/books/add', {
+				controller:'BookController',
+				controllerAs: 'vm',
+				templateUrl: 'templates/addBook.html'
+			})
+			.when('/books/:bookID', {
+				controller: 'BookDetailsController', // TODO: bookdetailscontroller
+				controllerAs: 'vm',
+				templateUrl: 'templates/bookDetails.html'
+			});
+	}
 
 	configuration.$inject = ['$routeProvider', '$compileProvider'];
 
 	module.config(configuration);
-
 
 })(angular.module('Bookstore'));
