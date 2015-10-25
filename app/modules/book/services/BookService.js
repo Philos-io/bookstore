35aa6c7901996ab75b91b719b724cd4b1a89b84f
davy engone <davy@philos.io>
2015-10-25 16:43:09 +0100

    fixed issue with bookservice

--- app/modules/book/services/BookService.js
+++ app/modules/book/services/BookService.js
@@ -1,66 +1,53 @@
-export default class BookFactory {
+export default function BookFactory($q, $http, Config){
 
-  constructor($q, $http, Config){}
+  return {
+    getAll(forced){
+      forced = forced  || false;
+      return $http.get(Config.BooksUrl, {cache: forced});
+    },
 
-  getAllFromServer(forced){
-    forced = forced  || false;
-    return $http.get(Config.BooksUrl, {cache: forced});
-  }
+    getCovers(){
+      return [
+        'images/angularjs.jpg',
+        'images/coman.jpeg',
+        'images/datavis.jpg',
+        'images/designpattern.jpg',
+        'images/emberjs.jpg',
+        'images/ionic.jpg',
+        'images/es6.jpg',
+        'images/iot.jpg'
+      ];
+    },
 
-  covers(){
-    return [
-      'images/angularjs.jpg',
-      'images/coman.jpeg',
-      'images/datavis.jpg',
-      'images/designpattern.jpg',
-      'images/emberjs.jpg',
-      'images/ionic.jpg',
-      'images/es6.jpg',
-      'images/iot.jpg'
-    ];
-  }
+    getCategories(){
+      let defer = $q.defer();
 
-  getAll(){
-    let defer = $q.defer();
+      getAll().then(success, error);
 
-    if (!books) {
-      defer.resolve({data: getBooks});
-    }else{
-      defer.reject({message:"things are broken!!"});
+      function success(response){
+        let books = response.data;
+        let categories = books.map(function(book){
+          return book.category;
+        });
+
+        defer.resolve(categories);
+      }
+
+      function error(err){
+        defer.reject({message: Config.Errors.NoCategory});
+      }
+      return defer.promise;
+    },
+
+    getBook(bookId){
+      let url = Config.BooksUrl + '/' + bookId;
+      return $http.get(url);
+    },
+
+    addBook(book){
+      return $http.post(Config.AddBookUrl, book);
     }
-
-    return defer.promise;
-  }
-
-  getCategories(){
-    let defer = $q.defer();
-
-    getAllFromServer().then(success, error);
-
-    success(response){
-      let books = response.data;
-      let categories = books.map(function(book){
-        return book.category;
-      });
-
-      defer.resolve(categories);
-    }
-
-    error(err){
-      defer.reject({message: Config.Errors.NoCategory});
-    }
-
-    return defer.promise;
-  }
-
-  getBook(bookId){
-    let url = Config.BooksUrl + '/' + bookId;
-    return $http.get(url);
-  }
-
-  addBook(book){
-    return $http.post(Config.AddBookUrl, book);
   }
 }
 
-bookfactory.$inject = ['$q', '$http', 'Config'];
+BookFactory.$inject = ['$q', '$http', 'Config'];
