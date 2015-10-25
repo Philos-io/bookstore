d2a3f70058750e83b19e76e7c426fb255a6db278
davy engone <davy@philos.io>
2015-10-25 16:43:31 +0100

    added angular-router and toastr

--- app/modules/book/index.js
+++ app/modules/book/index.js
@@ -1,14 +1,19 @@
 import angular , {module} from 'angular';
+import 'angular-route';
+import 'angular-toastr';
+
 
 import BookController from './controllers/BookController';
 import BookDetailsController from './controllers/BookDetailsController';
 import routes from './config/routes.js';
 import bookDirective from './directives/book';
 import bookStoreDirective from './directives/bookStore';
+import BookFactory from './services/BookService';
 
-module('book', [])
+module('book', ['ngRoute', 'toastr'])
   .config(routes)
   .controller('BookController', BookController)
   .controller('BookDetailsController', BookDetailsController)
   .directive('bsBook', bookDirective)
-  .directive('bsBookStore', bookStoreDirective);
+  .directive('bsBookStore', bookStoreDirective)
+  .factory('BookFactory', BookFactory);
