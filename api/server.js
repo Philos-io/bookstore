8df94d39ed890bc36e1cc188cec1498c01b2b587
davy engone <davy@philos.io>
2015-11-02 06:27:50 +0100

    changed path for webpack entry

--- api/server.js
+++ api/server.js
@@ -4,73 +4,73 @@ var express = require('express'),
 	bodyParser = require('body-parser');
 
 
-function getBooks(){
+  function getBooks(){
 
-            return [
-                {
-                  bookID: 9809,
-                  author: 'Davy Mitchel',
-                  title: 'Functional JavaScript',
-                  description: 'Functional programming in JavaScript',
-                  price: 29.99,
-                  category: 'Programming language',
-                  cover: 'images/javascript.jpg'
-                },
-                {
-                  bookID: 9100,
-                  author: 'Max Mitchel',
-                  title: 'AngularJS: Up and Running',
-                  description: 'Enhanced Productivity with Structured Web Apps',
-                  price: 19.99,
-                  category: 'Web Frameworks',
-                  cover: 'images/angularjs.jpg'
-                },
-                {
-                  bookID: 9353,
-                  author: 'Jesse Cravens, Thomas Q Brady',
-                  title: 'Introduction to IOT Programming with JavaScript',
-                  description: "In this book you will build robot, connected apps and devices applications.",
-                  price: 18.99,
-                  category: 'Web Frameworks',
-                  cover: 'images/iot.jpg'
-                },
-                {
-                  bookID: 9300,
-                  author: 'Jesse Cravens, Thomas Q Brady',
-                  title: 'Data visualisation in JavaScript',
-                  description:'Building data visualisation apps using JavaScript',
-                  price: 20.99,
-                  category: 'Web Frameworks',
-                  cover: 'images/datavis.jpg'
-                },
-                {
-                  bookID: 9657,
-                  author: 'Jesse Cravens, Thomas Q Brady',
-                  title: 'ES6: JavaScript Next',
-                  description: "This book will cover ES6 entirely and will show you how to use ES6 today.",
-                  price: 9.99,
-                  category: 'Web Frameworks',
-                  cover: 'images/es6.jpg'
-                },
-                {
-                  bookID: 9887,
-                  author: 'Jesse Cravens, Thomas Q Brady',
-                  title: 'Ionic Framework: Building hybrid app',
-                  description: "Build Hybrid apps using Angular, Ionic and deliver fast.",
-                  price: 9.99,
-                  category: 'Web Frameworks',
-                  cover: 'images/ionic.jpg'
-                },
-                {
-                  bookID: 9546,
-                  author: 'Backstop Media, Rick Waldron',
-                  title: 'Make: JavaScript Robotics',
-                  description: 'Building NodeBots with Raspberry Pi, Arduino, and BeagleBone',
-                  price: 10.99,
-                  category: 'Robotic & Programming',
-                  cover: 'images/robot.jpg'
-                }
-              ];}
+    return [
+      {
+        bookID: 9809,
+        author: 'Davy Mitchel',
+        title: 'Functional JavaScript',
+        description: 'Functional programming in JavaScript',
+        price: 29.99,
+        category: 'Programming language',
+        cover: 'images/javascript.jpg'
+      },
+      {
+        bookID: 9100,
+        author: 'Max Mitchel',
+        title: 'AngularJS: Up and Running',
+        description: 'Enhanced Productivity with Structured Web Apps',
+        price: 19.99,
+        category: 'Web Frameworks',
+        cover: 'images/angularjs.jpg'
+      },
+      {
+        bookID: 9353,
+        author: 'Jesse Cravens, Thomas Q Brady',
+        title: 'Introduction to IOT Programming with JavaScript',
+        description: "In this book you will build robot, connected apps and devices applications.",
+        price: 18.99,
+        category: 'Web Frameworks',
+        cover: 'images/iot.jpg'
+      },
+      {
+        bookID: 9300,
+        author: 'Jesse Cravens, Thomas Q Brady',
+        title: 'Data visualisation in JavaScript',
+        description:'Building data visualisation apps using JavaScript',
+        price: 20.99,
+        category: 'Web Frameworks',
+        cover: 'images/datavis.jpg'
+      },
+      {
+        bookID: 9657,
+        author: 'Jesse Cravens, Thomas Q Brady',
+        title: 'ES6: JavaScript Next',
+        description: "This book will cover ES6 entirely and will show you how to use ES6 today.",
+        price: 9.99,
+        category: 'Web Frameworks',
+        cover: 'images/es6.jpg'
+      },
+      {
+        bookID: 9887,
+        author: 'Jesse Cravens, Thomas Q Brady',
+        title: 'Ionic Framework: Building hybrid app',
+        description: "Build Hybrid apps using Angular, Ionic and deliver fast.",
+        price: 9.99,
+        category: 'Web Frameworks',
+        cover: 'images/ionic.jpg'
+      },
+      {
+        bookID: 9546,
+        author: 'Backstop Media, Rick Waldron',
+        title: 'Make: JavaScript Robotics',
+        description: 'Building NodeBots with Raspberry Pi, Arduino, and BeagleBone',
+        price: 10.99,
+        category: 'Robotic & Programming',
+        cover: 'images/robot.jpg'
+      }
+    ];}
 
 var books = getBooks();
 
@@ -88,7 +88,7 @@ app.get('/api/books', function(req, res){
 });
 
 app.get('/api/books/:bookID', function(req, res){
-    
+
     var bookID = Number(req.params.bookID);
 
     var book = books.filter(function(book){
@@ -111,7 +111,7 @@ app.post('/api/books/add', function(req, res){
 });
 
 app.put('/api/books/:bookID', function(req, res){
-    
+
     var bookID = Number(req.params.bookID), updatedBook = req.body, found = false;
 
     var books = getBooks();
@@ -126,7 +126,7 @@ app.put('/api/books/:bookID', function(req, res){
 
     if(!found){
         res.status(500).send({message: 'Update failed!'});
-    } 
+    }
 });
 
 app.listen(port, function(){
--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ var path = require('path');
 
 module.exports = {
   // devtool: 'source-map',
-  entry: './app/index',
+  entry: './angular1/index',
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, 'dist')
@@ -15,6 +15,6 @@ module.exports = {
     ]
   },
   devServer: {
-    contentBase: 'app'
+    contentBase: 'angular1'
   }
 };
