e56e234880ce0118fef934cd5f4a67b4d558b412
davy engone <davy@philos.io>
2015-11-14 18:56:08 +0100

    added shell directive as entry point of our app

--- app/index.js
+++ app/index.js
@@ -1,9 +1,11 @@
 import angular, {bootstrap, module} from 'angular';
+import 'angular-route';
 
 // Import core module
 import './modules/core';
 import './modules/book';
+import './routes';
 
-module('bookstore', ['core', 'book']);
+module('bookstore', ['bookstore.routes', 'core', 'book'])
 
 bootstrap(document.body, ['bookstore']);
