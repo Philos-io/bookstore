4049cd258a6907e9e4e45e1433ec0c59899267ec
davy engone <davy@philos.io>
2015-11-19 09:50:31 +0100

    moved the apps routes into its own module

---
+++ app/modules/routes/index.js
@@ -0,0 +1,29 @@
+import angular, {module} from 'angular';
+import 'angular-route';
+
+export default function routes($routeProvider, $compileProvider){
+
+  // $compileProvider.debugInfoEnabled && $compileProvider.debugInfoEnabled(false);
+
+  $routeProvider
+      .when('/', {
+        template: '<books></books>'
+      })
+      .when('/books', {
+        template: '<books></books>'
+      })
+      .when('/books/add', {
+        controller:'BookController',
+        controllerAs: 'vm',
+        templateUrl: 'templates/addBook.html'
+      })
+      .when('/books/:bookID', {
+        controller: 'BookDetailsController',
+        controllerAs: 'vm',
+        templateUrl: 'templates/bookDetails.html'
+      });
+}
+
+routes.$inject = ['$routeProvider', '$compileProvider'];
+
+module('bookstore.routes', ['ngRoute']).config(routes);
